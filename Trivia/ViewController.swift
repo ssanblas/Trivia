c27a3efc2bd7b25e27a73bd05b3568d629bd848d
Sebastian San Blas <sebastian.sanblas@mercadolibre.com>
2021-10-26 20:50:29 -0300

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Sebastian San Blas on 26/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
